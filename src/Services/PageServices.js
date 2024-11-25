81c3d41c594c81e44e3a20292e91b00fdd83556a
Usman Kapadiya <hirenk@intellidt.com>
2024-11-25 13:31:29 +0530

    get pages list api integrate but Api in some error show

--- src/Services/PageServices.js
+++ src/Services/PageServices.js
@@ -4,6 +4,12 @@ const PageServices = {
     uploadPages:  async (body) => {
         return requests.post("/page/upload", body);
       },
+      getPageList: async({ page,perPageRecords,documentType}) =>{
+        console.log("page",page);
+        console.log("perPageRecords",perPageRecords);
+        console.log("documentType",documentType);
+        return requests.post(`/page/list?page=${page}&pageSize=${perPageRecords}&type=${documentType}`,);
+      },
 
 }
 export default PageServices;
--- src/pages/Pages/Pages.js
+++ src/pages/Pages/Pages.js
@@ -4,23 +4,60 @@ import Navbar from '../Component/Navbar/Navbar';
 import Button from "../Component/ButtonComponents/ButtonComponents";
 import { useNavigate } from "react-router-dom";
 import Skeleton from "../Component/SkeletonComponent/SkeletonComponent";
+import PageServices from "../../Services/PageServices";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Pages = () => {
   const navigate = useNavigate();
-
+  const [loading, setLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1);
+  const perPageRecords = (10)
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setpage] = useState([]);
   const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
+  const [documentType, setDocumentType] = useState("all");
+
 
   const toggleSidebar = () => {
     setIsSidebarVisible(!isSidebarVisible);
   };
   useEffect(() => {
-    console.log('component mounted');
+    setLoading(true)
+    fetchPages(currentPage,documentType);
   }, []);
   const redirectToCreatePage = () => {
     navigate('/uploadDocument');
+  };
+  const fetchPages = async (page, documentType) => {
+    try {
+      // const formdata = new FormData();
+      // formdata.append("page", page);
+      // formdata.append("pageSize", perPageRecords);
+
+      const resp = await PageServices.getPageList({page,perPageRecords,documentType });
+      if (resp?.status_code === 200) {
+        console.log(resp);
+
+        if (page === 1) {
+          // setMediaList(resp?.list?.data || []);
+        } else {
+          // setMediaList((prevList) => [...prevList, ...resp?.list?.data]);
+        }
+
+        // setCurrentPage(resp?.list?.current_page);
+        // setHasMore(resp?.list?.next_page_url !== null);
+
+        // setTimeout(() => handleClose(), 3000);
+      } else {
+        toast.error("Please try again.", { position: "top-center", autoClose: 3000 });
+      }
+    } catch (error) {
+      console.error("Error uploading images:", error);
+      toast.error("An error occurred during fetch Data. Please try again.", { position: "top-center", autoClose: 3000 });
+    } finally {
+      setLoading(false);
+    }
   };
   useEffect(() => {
     // Simulate data fetch
@@ -74,14 +111,14 @@ const Pages = () => {
             {/* Dashboard Content */}
             <div className="dashboard-content">
               <div className="container-fluid">
-                    <Button
-                      onClick={redirectToCreatePage}
-                      text="Upload New Document"
-                      className="btn btn-primary"
-                      type="button"
-                    >
-                      Upload New Document
-                    </Button>
+                <Button
+                  onClick={redirectToCreatePage}
+                  text="Upload New Document"
+                  className="btn btn-primary"
+                  type="button"
+                >
+                  Upload New Document
+                </Button>
 
                 <div className="px-2 mb-3 mt-5 row d-flex justify-content-between">
                   <div className="custom-select-wrapper col-md-3 p-1">
@@ -92,10 +129,16 @@ const Pages = () => {
                   </div>
 
                   <div className="custom-select-wrapper col-md-3 p-1">
-                    <select id="postCategories" className="form-control" style={{ height: 'auto' }}>
-                      <option value="">All Categories</option>
-                      <option value="news">News</option>
-                      <option value="visit">Visit</option>
+                    <select
+                      id="postCategories"
+                      className="form-control"
+                      style={{ height: 'auto' }}
+                      value={documentType}
+                      onChange={(e) => setDocumentType(e.target.value)}>
+                      <option value="all">All Categories</option>
+                      <option value="publications">Publications</option>
+                      <option value="hedgeFundReports">Hedge Fund Reports</option>
+                      <option value="managedAccountReports">Managed Account Reports</option>
                     </select>
                   </div>
                   <div className="custom-select-wrapper col-md-3 p-1">
@@ -141,52 +184,52 @@ const Pages = () => {
 
                 <table className="table table-striped" id="user-data-table" style={{ border: '1px solid #ccc' }}>
                   <thead>
-                  <tr>
-                    <th><input type="checkbox" id="select-all" /></th>
-                    <th>Document Title</th>
-                    <th>Document types</th>
-                    <th>Years</th>
-                    <th>Actions</th>
-                  </tr>
+                    <tr>
+                      <th><input type="checkbox" id="select-all" /></th>
+                      <th>Document Title</th>
+                      <th>Document types</th>
+                      <th>Years</th>
+                      <th>Actions</th>
+                    </tr>
                   </thead>
                   <tbody>
-                  {isLoading ? (
-                    <>
-                      <Skeleton type="row" />
-                      <Skeleton type="row" />
-                      <Skeleton type="row" />
-                    </>
-                  ) : page.length > 0 ? (
-                    page.map((page) => (
-                      <tr key={page.id}>
-                        <td>
-                          <input
-                            type="checkbox"
-                            className="user-select"
-                            checked={selectedCheckboxes.includes(page.id)}
-                            onChange={(e) =>
-                              handleCheckboxChange(page.id, e.target.checked)
-                            }
-                          />
-                        </td>
-                        <td>{page.title}</td>
-                        <td>{page.type}</td>
-                        <td>{page.year}</td>
-                        <td>
-                          <button
-                            className="btn btn-primary ml-3"
-                            onClick={redirectToCreatePage}
-                          >
-                            Edit
-                          </button>
-                        </td>
+                    {isLoading ? (
+                      <>
+                        <Skeleton type="row" />
+                        <Skeleton type="row" />
+                        <Skeleton type="row" />
+                      </>
+                    ) : page.length > 0 ? (
+                      page.map((page) => (
+                        <tr key={page.id}>
+                          <td>
+                            <input
+                              type="checkbox"
+                              className="user-select"
+                              checked={selectedCheckboxes.includes(page.id)}
+                              onChange={(e) =>
+                                handleCheckboxChange(page.id, e.target.checked)
+                              }
+                            />
+                          </td>
+                          <td>{page.title}</td>
+                          <td>{page.type}</td>
+                          <td>{page.year}</td>
+                          <td>
+                            <button
+                              className="btn btn-primary ml-3"
+                              onClick={redirectToCreatePage}
+                            >
+                              Edit
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan="5">No page available yet.</td>
                       </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan="5">No page available yet.</td>
-                    </tr>
-                  )}
+                    )}
                   </tbody>
                 </table>
               </div>
