6375b17fb8dc04fb693e882d542e30b691042f66
Usman Kapadiya <hirenk@intellidt.com>
2024-11-25 15:57:54 +0530

    add logout functionality"

--- src/pages/Component/Navbar/Navbar.js
+++ src/pages/Component/Navbar/Navbar.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Navbar = ({ toggleSidebar }) => {
+  const location = useLocation();
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userData');
+
+    toast.success('You have logged out successfully!', {
+      position: "top-center",
+      autoClose: 3000,
+    });
+
+    setTimeout(() => {
+      window.location.href = '/';
+    }, 3000);
+  };
   return (
+    <React.Fragment>
     <nav className="navbar navbar-expand-lg customWhiteBg shadow-sm">
       <a className="navbar-brand ps-2" href="#">My Admin</a>
       <button
@@ -16,17 +33,25 @@ const Navbar = ({ toggleSidebar }) => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
+      <div className="collapse navbar-collapse me-2" id="navbarNav">
+        <ul className="navbar-nav ms-auto"> {/* Add ms-auto to push to the right */}
+        {location.pathname !== '/profile' && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+            </li>
+          )}
           <li className="nav-item">
-            <a className="nav-link" href="#">Profile</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Logout</a>
+            <a className="nav-link" href="#" onClick={handleLogout}>Logout</a>
           </li>
         </ul>
       </div>
+    
     </nav>
+    <ToastContainer />
+    </React.Fragment>
+
   );
 };
 
