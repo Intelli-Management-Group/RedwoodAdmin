954f8fb575efed2cd17f29a6c0d27c4e66f38dae
Usman Kapadiya <hirenk@intellidt.com>
2024-12-05 10:24:18 +0530

    pagination set

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -73,7 +73,7 @@ const Post = () => {
 
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
-        // fetchPost(pageNumber, documentType);
+        fetchPost(pageNumber);
 
     };
 
